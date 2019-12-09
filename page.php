2860ea990f21469f63a4e52b73cc26f6dc849a73
Mark Wilde <markwilde29@gmail.com>
2019-12-09 16:06:39 +0000

    Added functions.php, added some functionality across templates.

--- page.php
+++ page.php
@@ -1,10 +1,10 @@
 <?php
-
+get_header();
  while(have_posts()) {
    the_post();?>
    <h2>This is a page<br /><hr><?php the_title(); ?></h2>
    <p><?php the_content(); ?></p>
 <?php 
 }
-
+get_footer();
 ?>
