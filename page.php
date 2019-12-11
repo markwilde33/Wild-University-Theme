35b616139aedabe2488fcb363f37e182d9d5047d
Mark Wilde <markwilde29@gmail.com>
2019-12-11 10:49:37 +0000

    Added the menu for chilg page links, wrote the php so that the approriate menus will display based off the current child page.

--- page.php
+++ page.php
@@ -24,16 +24,32 @@ get_header();
      <?php
     }
     ?>
-  
     
-    
-    <!-- <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
+    <?php
+    $pishMicheal = get_pages(array(
+      'child_of' => get_the_ID()
+    ));
+
+    if ($parentPage or $pishMicheal) { ?>
+    <div class="page-links">
+      <h2 class="page-links__title"><a href="#"><?php echo get_the_title($parentPage); ?></a></h2>
       <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
+       <?php
+         if($parentPage) {
+           $getChildren = $parentPage;
+         } else{
+          $getChildren = get_the_ID();
+         }
+
+        wp_list_pages(array(
+          'title_li' => NULL,
+          'child_of' => $getChildren,
+          'sort_column' => 'menu_order'
+        ));
+       ?>
       </ul>
-    </div> -->
+    </div>
+      <?php } ?>
 
     <div class="generic-content">
     <?php the_content(); ?>
