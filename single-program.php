4bdcf7784fcaf6e8e1cd49844a3c0c1228a0fced
Mark Wilde <markwilde29@gmail.com>
2019-12-19 12:04:19 +0000

    Reused code block showing events related to a program to also show professors related to a program.

--- single-program.php
+++ single-program.php
@@ -20,6 +20,32 @@ get_header();
     </div>
     <div class="generic-content"><?php the_content(); ?></div>
     <?php
+
+$relatedProfessors = new WP_Query(array(
+  'posts_per_page' => -1,
+  'post_type' => 'professor',
+  'orderby' => 'title',
+  'order' => 'ASC',
+  'meta_query' => array(
+    array(
+      'key' => 'related_programs',
+      'compare' => 'LIKE',
+      'value' => '"' . get_the_ID() . '"'
+    )
+  )
+));
+
+if ($relatedProfessors->have_posts()) { ?>
+  <hr class="section-break">
+  <h2 class="headline headline--medium"><?php echo get_the_title(); ?> Professors</h2>
+  <?php
+  while($relatedProfessors-> have_posts()) {
+    $relatedProfessors->the_post(); ?>
+    <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+  <?php } wp_reset_postdata();
+  ?>
+<?php } 
+
         $today = date('Ymd');
         $homepageEvents = new WP_Query(array(
           'posts_per_page' => 2,
