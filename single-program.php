ca6d9991d5195ce57a608f89529d9be7f14568c4
Mark Wilde <markwilde29@gmail.com>
2019-12-20 17:10:42 +0000

    Added images of each professor with links to their single page bio.

--- single-program.php
+++ single-program.php
@@ -38,12 +38,19 @@ $relatedProfessors = new WP_Query(array(
 if ($relatedProfessors->have_posts()) { ?>
   <hr class="section-break">
   <h2 class="headline headline--medium"><?php echo get_the_title(); ?> Professors</h2>
+  <ul class="professor-cards">
   <?php
   while($relatedProfessors-> have_posts()) {
     $relatedProfessors->the_post(); ?>
-    <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+    <li class="professor-card__list-item">
+      <a class="professor-card" href="<?php the_permalink(); ?>">
+        <img class="professor-card__image" src="<?php the_post_thumbnail_url(); ?>">
+        <span class="professor-card__name"><?php the_title(); ?></span>
+      </a>
+    </li>
   <?php } wp_reset_postdata();
   ?>
+  </ul>
 <?php } 
 
         $today = date('Ymd');
