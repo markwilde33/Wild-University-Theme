5276827f3a2f523c9c0ab8f7009d20d23db443b2
Mark Wilde <markwilde29@gmail.com>
2019-12-22 19:19:39 +0000

    Added some additional images which are being used in different post types. Cleaned up code across pages.

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
 </head>
 <body <?php body_class(); ?>>
 <header class="site-header">
+
     <div class="container">
       <h1 class="school-logo-text float-left"><a href="<?php echo site_url('/home') ?>"><strong>Wild</strong> University</a></h1>
       <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
@@ -22,11 +23,14 @@
             <li <?php if (get_post_type() == 'post') echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/blog') ?>">Blog</a></li>
           </ul>
         </nav>
+
         <div class="site-header__util">
           <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
           <a href="#" class="btn btn--small  btn--dark-orange float-left">Sign Up</a>
           <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
         </div>
+        
       </div>
     </div>
-  </header>
+
+</header>
