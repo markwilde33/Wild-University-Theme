92a51511c60b1578e5b4ee5b7cc6212e098f5c3e
Mark Wilde <markwilde29@gmail.com>
2019-12-22 19:09:17 +0000

    Deleted the page banner div and used the pageBanner function in it's place, minor code clean up also.

--- single-program.php
+++ single-program.php
@@ -3,102 +3,104 @@
 get_header();
 
  while(have_posts()) {
-   the_post();?>
-   <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+  the_post();?>
+  <div class="page-banner">
+   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/mountain.jpg') ?>);"></div>
     <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title"><?php the_title(); ?></h1>
-      <div class="page-banner__intro">
-        <p>*Update this message later</p>
-      </div>
-    </div>  
+    <h1 class="page-banner__title"><?php the_title(); ?></h1>
+    <div class="page-banner__intro">
+    <p>*Update this message later</p>
+    </div>
+   </div>  
   </div>
 
   <div class="container container--narrow page-section">
-  <div class="metabox metabox--position-up metabox--with-home-link">
+   <div class="metabox metabox--position-up metabox--with-home-link">
       <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program'); ?>"><i class="fa fa-home" aria-hidden="true"></i> All Programs</a> <span class="metabox__main"><?php the_title(); ?> </span></p>
-    </div>
-    <div class="generic-content"><?php the_content(); ?></div>
-    <?php
+   </div>
+   <div class="generic-content"><?php the_content(); ?></div>
+   <?php
 
-$relatedProfessors = new WP_Query(array(
-  'posts_per_page' => -1,
-  'post_type' => 'professor',
-  'orderby' => 'title',
-  'order' => 'ASC',
-  'meta_query' => array(
-    array(
-      'key' => 'related_programs',
-      'compare' => 'LIKE',
-      'value' => '"' . get_the_ID() . '"'
+   $relatedProfessors = new WP_Query(array(
+     'posts_per_page' => -1,
+     'post_type' => 'professor',
+     'orderby' => 'title',
+     'order' => 'ASC',
+     'meta_query' => array(
+     array(
+       'key' => 'related_programs',
+       'compare' => 'LIKE',
+       'value' => '"' . get_the_ID() . '"'
+     )
     )
-  )
-));
+   ));
 
-if ($relatedProfessors->have_posts()) { ?>
-  <hr class="section-break">
-  <h2 class="headline headline--medium"><?php echo get_the_title(); ?> Professors</h2>
-  <ul class="professor-cards">
-  <?php
-  while($relatedProfessors-> have_posts()) {
-    $relatedProfessors->the_post(); ?>
-    <li class="professor-card__list-item">
-      <a class="professor-card" href="<?php the_permalink(); ?>">
+   if ($relatedProfessors->have_posts()) { ?>
+    <hr class="section-break">
+    <h2 class="headline headline--medium"><?php echo get_the_title(); ?> Professors</h2>
+    <ul class="professor-cards">
+     <?php
+
+     while($relatedProfessors-> have_posts()) {
+      $relatedProfessors->the_post(); ?>
+      <li class="professor-card__list-item">
+       <a class="professor-card" href="<?php the_permalink(); ?>">
         <img class="professor-card__image" src="<?php the_post_thumbnail_url('professorLandscape'); ?>">
         <span class="professor-card__name"><?php the_title(); ?></span>
-      </a>
-    </li>
-  <?php } wp_reset_postdata();
-  ?>
-  </ul>
-<?php } 
+       </a>
+      </li>
+     <?php } wp_reset_postdata();
+     ?>
+    </ul>
+    <?php } 
 
-        $today = date('Ymd');
-        $homepageEvents = new WP_Query(array(
-          'posts_per_page' => 2,
-          'post_type' => 'event',
-          'meta_key' => 'event_date',
-          'orderby' => 'meta_value_num',
-          'order' => 'ASC',
-          'meta_query' => array(
-            array(
-              'key' => 'event_date',
-              'compare' => '>=',
-              'value' => $today,
-              'type' => 'numeric'
-            ),
-            array(
-              'key' => 'related_programs',
-              'compare' => 'LIKE',
-              'value' => '"' . get_the_ID() . '"'
-            )
+     $today = date('Ymd');
+      $homepageEvents = new WP_Query(array(
+        'posts_per_page' => 2,
+        'post_type' => 'event',
+        'meta_key' => 'event_date',
+        'orderby' => 'meta_value_num',
+        'order' => 'ASC',
+        'meta_query' => array(
+          array(
+            'key' => 'event_date',
+            'compare' => '>=',
+             'value' => $today,
+             'type' => 'numeric'
+          ),
+          array(
+            'key' => 'related_programs',
+            'compare' => 'LIKE',
+            'value' => '"' . get_the_ID() . '"'
           )
+         )
         ));
 
         if ($homepageEvents->have_posts()) { ?>
           <hr class="section-break">
           <h2 class="headline headline--medium">Upcoming <?php echo get_the_title(); ?> Events</h2>
-          <?php
-          while($homepageEvents-> have_posts()) {
-            $homepageEvents->the_post(); ?>
-            <div class="event-summary">
+        <?php
+
+        while($homepageEvents-> have_posts()) {
+          $homepageEvents->the_post(); ?>
+          <div class="event-summary">
             <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-              <span class="event-summary__month"><?php 
-              $eventDate = new DateTime(get_post_field('event_date'));
-              echo $eventDate->format('M')
-              ?></span>
-              <span class="event-summary__day"><?php 
-              echo $eventDate->format('d')
-              ?></span>  
+            <span class="event-summary__month"><?php 
+            $eventDate = new DateTime(get_post_field('event_date'));
+            echo $eventDate->format('M')
+            ?></span>
+            <span class="event-summary__day"><?php 
+            echo $eventDate->format('d')
+            ?></span>  
             </a>
             <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-              <p><?php if (has_excerpt()) {
-                echo get_the_excerpt();
-              } else {
-                echo wp_trim_words(get_the_content(),13);
-              }?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-            </div>
+             <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+             <p><?php if (has_excerpt()) {
+               echo get_the_excerpt();
+             } else {
+               echo wp_trim_words(get_the_content(),13);
+             }?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+             </div>
           </div>
           <?php } wp_reset_postdata();
           ?>
