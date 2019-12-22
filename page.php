efc17a4dd0e03f3bf1e2184c0e25c20b71a03c1a
Mark Wilde <markwilde29@gmail.com>
2019-12-22 16:48:16 +0000

    Added pageBanner function and deleted the page-banner div, cleaned up code.

--- page.php
+++ page.php
@@ -1,25 +1,17 @@
 <?php
 get_header();
  while(have_posts()) {
-   the_post();?>
+   the_post();
+   pageBanner();
+   ?>
    
-   <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
-    <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title"><?php the_title(); ?></h1>
-      <div class="page-banner__intro">
-        <p>*Update this message later</p>
-      </div>
-    </div>  
-  </div>
-
-  <div class="container container--narrow page-section">
-
+<div class="container container--narrow page-section">
   <?php
   $parentPage = wp_get_post_parent_id(get_the_ID());
     if ($parentPage) {?>
      <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($parentPage); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($parentPage); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
+      <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($parentPage); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($parentPage); ?></a> <span class="metabox__main"><?php the_title(); ?></span>
+      </p>
     </div>
      <?php
     }
@@ -54,10 +46,9 @@ get_header();
     <div class="generic-content">
     <?php the_content(); ?>
     </div>
-
-  </div>
+</div>
 
 <?php 
 }
-get_footer();
-?>
+get_footer();?>
+
