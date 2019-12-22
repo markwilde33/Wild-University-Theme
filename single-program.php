23a89f5a69be1e5b1f5f529c695717f54fada6ad
Mark Wilde <markwilde29@gmail.com>
2019-12-22 19:14:56 +0000

    Last save hadn't been edited properlu, photo field also added to pageBanner.

--- single-program.php
+++ single-program.php
@@ -3,16 +3,12 @@
 get_header();
 
  while(have_posts()) {
-  the_post();?>
-  <div class="page-banner">
-   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/mountain.jpg') ?>);"></div>
-    <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title"><?php the_title(); ?></h1>
-    <div class="page-banner__intro">
-    <p>*Update this message later</p>
-    </div>
-   </div>  
-  </div>
+  the_post();
+   
+  pageBanner(array(
+    'photo' => get_theme_file_uri('/images/mountain.jpg')
+  ));
+  ?>
 
   <div class="container container--narrow page-section">
    <div class="metabox metabox--position-up metabox--with-home-link">
