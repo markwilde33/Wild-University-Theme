a5eb24d493b3f24242c3a64f25629f71eb6156c6
Mark Wilde <markwilde29@gmail.com>
2019-12-23 18:34:05 +0000

    Added a function to return the google maps API

--- functions.php
+++ functions.php
@@ -1,5 +1,4 @@
 <?php
-
 function pageBanner($args = NULL) {
   if (!$args['title']) {
     $args['title'] = get_the_title();
@@ -26,16 +25,13 @@ function pageBanner($args = NULL) {
   </div>
 <?php
 }
-
 function university_files(){
    wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), Null, 'microtime()', true);
    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
    wp_enqueue_style('university_main_styles', get_stylesheet_uri(), NULL, 'microtime()');
 }
-
 add_action('wp_enqueue_scripts', 'university_files');
-
 function university_features(){
    add_theme_support('title-tag');
    add_theme_support('post-thumbnails');
@@ -43,16 +39,13 @@ function university_features(){
    add_image_size('professorPortrait', 480, 650, true);
    add_image_size('pageBanner', 1500, 350, true);
 }
-
 add_action('after_setup_theme', 'university_features');
-
 function university_adjust_queries($query){
  if(!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()) {
    $query->set('orderby', 'title');
    $query->set('order', 'ASC');
    $query->set('posts_per_page', -1);
  }
-
  if(!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
    $today = date('Ymd');
    $query->set('meta_key', 'event_date');
@@ -68,5 +61,11 @@ function university_adjust_queries($query){
     ));
  }
 }
-
 add_action('pre_get_posts', 'university_adjust_queries');
+
+function universityMapKey($api) {
+  $api['key'] = GOOGLE_MAPS_API_KEY;
+  return $api;
+}
+
+add_filter('acf/fields/google_map/api', 'universityMapKey');
