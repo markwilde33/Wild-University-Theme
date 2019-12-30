91d338ba52593813d4ab0799c8a7bcfa16fcf90b
Mark Wilde <markwilde29@gmail.com>
2019-12-30 14:30:36 +0000

    Deleted unused code form single-campus, added an if statement to function university_adjust_queries; maps will show multipe campus pins.

--- functions.php
+++ functions.php
@@ -26,13 +26,14 @@ function pageBanner($args = NULL) {
 <?php
 }
 function university_files(){
-   wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=GOOGLE_MAPS_API_KEY', Null, '1.0', true);
+   wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=', Null, '1.0', true);
    wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), Null, 'microtime()', true);
    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
    wp_enqueue_style('university_main_styles', get_stylesheet_uri(), NULL, 'microtime()');
 }
 add_action('wp_enqueue_scripts', 'university_files');
+
 function university_features(){
    add_theme_support('title-tag');
    add_theme_support('post-thumbnails');
@@ -41,7 +42,12 @@ function university_features(){
    add_image_size('pageBanner', 1500, 350, true);
 }
 add_action('after_setup_theme', 'university_features');
+
 function university_adjust_queries($query){
+  if(!is_admin() AND is_post_type_archive('campus') AND $query->is_main_query()) {
+    $query->set('posts_per_page', -1);
+  }
+
  if(!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()) {
    $query->set('orderby', 'title');
    $query->set('order', 'ASC');
@@ -65,7 +71,7 @@ function university_adjust_queries($query){
 add_action('pre_get_posts', 'university_adjust_queries');
 
 function universityMapKey($api) {
-  $api['key'] = 'GOOGLE_MAPS_API_KEY';
+  $api['key'] = GOOGLE_MAPS_API_KEY;
   return $api;
 }
 
